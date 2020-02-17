a792318b655bbd83ac5336b1216b8e2ea6020c64
migueruokumura <migueruokumura@gmail.com>
2020-02-17 12:57:29 -0500

    Update modulaciones_digital.m

--- modulaciones/modulaciones_digital.m
+++ modulaciones/modulaciones_digital.m
@@ -1,61 +1,84 @@
-clc, clear all, close all
+clc
+clear all
+close all
 
 %input
 
-datos='00110100010';
+datos = '00110100010';
 regla_bit_alto = 1;
-%carrier
-Ac=10;
+
+
+%Carrier
+Ac= 10;
 fc=1000e3;
-thetac=0;
+thetac =0;
 cpc=15;
 
-%process
 
-ascii = double(datos);
-bits = dec2bin(ascii);
-cadena = reshape(bits',1,[]); %' al final es matriz traspuesta ;[]  para calcular la salida de bits (4*7)
+%ask
+
+regla_ask_tr_carrier=1;
+
+
+%procesos
 
 %graficar cadena de funciones
-tb=100 %duracion del bit
-uno = ones(1,tb) %matriz de unos
-cero = zeros(1,tb) %matriz de ceros
+tb=100; %duracion del bit
+uno = ones(1,tb); %matriz de unos
+cero = zeros(1,tb); %matriz de ceros
 frame=[];
 frameInv=[];
 list=[];
 f='0';
-for n=1:length(cadena)
-    if (cadena(n)=='1')
+for n=1:length(datos)
+    if (datos(n)=='1')
         frame = [ frame uno ]; %vector que se autoacumula
-        frameInv = [frameInv cero] 
+        frameInv = [frameInv cero] ;
     else 
         frame = [ frame cero ];
-        frameInv = [frameInv cero]
+        frameInv = [frameInv uno];
     end
-    if (cadena(n)~= f)
-        f=cadena(n);
+    if (datos(n)~= f)
+        f=datos(n);
         list = [list length(frame)];
     end
 end
 if(regla_bit_alto==1)
-    cadenaBits=5*frame %0 a 5 voltios
+    cadenaBits=5*frame; %0 a 5 voltios
 else
-    cadenaBIts=5*frameInv
+    cadenaBits=5*frameInv;
 end
-%carrier
-tc=linspace(0,length(datos)/fc,500);
-carrier= Ac*sin(2*pi*fc*tc+thetac)
+
+tc = linspace(0, 2*length(datos)/fc,length(cadenaBits));
+carrier = Ac* sin(2*pi*fc*tc+thetac);
+
+if(regla_ask_tr_carrier == 1)
+    ask = frame.*carrier;
+else
+    ask = frameInv.*carrier;
+end
 
 %output
 
-titulo=cat(2,'Datos: ',datos)
+frame;
+
+%para visualizar grafica
 figure(1)
-subplot(2,1,1),plot(frame)
+subplot(3,1,1),plot(cadenaBits)
 xticks(list-100)
-axis([0 length(frame) -1 6])
+axis([0 length(cadenaBits) -1 6])
+title('Datos')
 grid on
-subplot(2,1,2),plot(carrier) 
+
+subplot(3,1,2),plot(carrier) 
 xticks(list-100)
-axis([0 length(carrier) -1 6])
-title('cadena de bits')
+axis([0 length(cadenaBits) -10 10])
+title('cadena de carrier')
+grid on
+
+
+subplot(3,1,3),plot(ask) 
+xticks(list-100)
+axis([0 length(cadenaBits) -10 10])
+title('cadena ask')
 grid on
